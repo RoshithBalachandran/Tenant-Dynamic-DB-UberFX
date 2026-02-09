6c7c79daac82bc378d40ae32a3758067ec5ff645
RoshithBalachandran <roshith9745@gmail.com>
2026-02-09 10:23:03 +0530

    update repo

--- internals/repository/repository.go
+++ internals/repository/repository.go
@@ -17,17 +17,6 @@ func (r *UserRepo) Create(db *gorm.DB, user *models.User) error {
 	return db.Create(user).Error
 }
 
-func (r *UserRepo) FindByEmail(db *gorm.DB, email string) (*models.User, error) {
-	var user models.User
-
-	err := db.Where("email = ?", email).First(&user).Error
-	if err != nil {
-		return nil, errors.New("user not registered")
-	}
-
-	return &user, nil
-}
-
 func (r *UserRepo) GetAll(db *gorm.DB) ([]models.User, error) {
 	var users []models.User
 	err := db.Find(&users).Error
